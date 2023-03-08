84cdbcbae5c38cdf34bbfbb9886dd5e13de2209e
Ahmed <ahmed.sattar@live.com>
2023-03-08 21:56:49 +0600

    first commit

---
+++ src/components/Brand.jsx
@@ -0,0 +1,24 @@
+import "./styles/brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "../assets/_index";
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} />
+    </div>
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={atlassian} />
+    </div>
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div>
+  </div>
+);
+
+export default Brand;
